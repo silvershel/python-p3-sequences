c3ce53defc27ddc926051b27fc430d82b3a75db1
Shelli Martinez <shelli.mrtnz@gmail.com>
2024-05-28 08:47:48 -0400

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    elif length >= 3:
+        fibs = [0, 1]
+        for i in range(2, length):
+            fibs.append(fibs[i-1] + fibs[i-2])
+        print(fibs)
